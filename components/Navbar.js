f5380e3462933e103c8101cd2dd8922a103edd66
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-09-16 16:58:10 +0530

    creating navbar, footer & homepage

---
+++ components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+
+const Navbar = () => {
+  return (
+    <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2'>
+      <div className="logo mx-5">
+        <Image src={'/logo.webp'} alt='' width={200} height={40} />
+      </div>
+      <div className="nav">
+        <ul className='flex items-center space-x-2 font-bold md:text-xl'>
+          <Link href={"/tshirts"}><a><li>Tshirts</li></a></Link>
+          <Link href={'/hoodies'}><a><li>Hoodies</li></a></Link>
+          <Link href={'/stickers'}><a><li>Stickers</li></a></Link>
+          <Link href={'/mugs'}><a><li>Mugs</li></a></Link>
+        </ul>
+      </div>
+      <div className="cart absolute right-0 mx-5 top-4">
+        <AiOutlineShoppingCart className='text-xl md:text-3xl'/>
+      </div>
+    </div>
+  ) 
+}
+
+export default Navbar
