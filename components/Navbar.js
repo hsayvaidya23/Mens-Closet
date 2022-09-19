c6e1a70c6a856d5a9c3d69b0737953ad63fa083a
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-09-20 00:21:37 +0530

    designing the cart sidebar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,29 +1,135 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { AiOutlineShoppingCart } from 'react-icons/ai';
+import React, { useRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  AiOutlineShoppingCart,
+  AiFillCloseCircle,
+  AiFillPlusCircle,
+  AiFillMinusCircle,
+} from "react-icons/ai";
+import { BsFillBagCheckFill } from "react-icons/bs";
 
 const Navbar = () => {
+  const toggleCart = () => {
+    if (ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-full");
+      ref.current.classList.add("translate-x-0");
+    } else if (!ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-0");
+      ref.current.classList.add("translate-x-full");
+    }
+  };
+  const ref = useRef();
   return (
-    <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md mb-1'>
-
+    <div className="flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md mb-1">
       <div className="logo mx-5">
-        <Link href={'/'}><a><Image src={'/logo.webp'} alt='' width={200} height={40} /></a></Link>
-
+        <Link href={"/"}>
+          <a>
+            <Image src={"/logo.webp"} alt="" width={200} height={40} />
+          </a>
+        </Link>
       </div>
       <div className="nav">
-        <ul className='flex items-center space-x-6 font-bold md:text-md'>
-          <Link href={"/tshirts"}><a><li>Tshirts</li></a></Link>
-          <Link href={'/hoodies'}><a><li>Hoodies</li></a></Link>
-          <Link href={'/stickers'}><a><li>Stickers</li></a></Link>
-          <Link href={'/mugs'}><a><li>Mugs</li></a></Link>
+        <ul className="flex items-center space-x-6 font-bold md:text-md">
+          <Link href={"/tshirts"}>
+            <a>
+              <li>Tshirts</li>
+            </a>
+          </Link>
+          <Link href={"/hoodies"}>
+            <a>
+              <li>Hoodies</li>
+            </a>
+          </Link>
+          <Link href={"/stickers"}>
+            <a>
+              <li>Stickers</li>
+            </a>
+          </Link>
+          <Link href={"/mugs"}>
+            <a>
+              <li>Mugs</li>
+            </a>
+          </Link>
         </ul>
       </div>
-      <div className="cart absolute right-0 mx-5 top-4">
-        <AiOutlineShoppingCart className='text-xl md:text-2xl' />
+      <div
+        onClick={toggleCart}
+        className=" cursor-pointer cart absolute right-0 mx-5 top-4"
+      >
+        <AiOutlineShoppingCart className="text-xl md:text-2xl" />
+      </div>
+
+      <div
+        ref={ref}
+        className="w-60 h-full sideCart absolute top-0 right-0 bg-pink-100 px-8 py-10 transform transition-transform translate-x-full z-10"
+      >
+        <h2 className="font-bold text-xl text-center">Shopping cart</h2>
+        <span
+          onClick={toggleCart}
+          className="absolute top-5 right-2 cursor-pointer text-2xl text-pink-500"
+        >
+          <AiFillCloseCircle />
+        </span>
+        <ol className="list-decimal font-semibold">
+          <li>
+            <div className="item flex my-5">
+              <div className="w-2/3 font-semibold">Tshirt - Wear the code</div>
+              <div className="w-1/3 flex items-center justify-center text-lg">
+                <AiFillMinusCircle className="cursor-pointer text-pink-500" />
+                <span className="mx-2 text-sm">1</span>
+                <AiFillPlusCircle className="cursor-pointer text-pink-500" />
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="item flex my-5">
+              <div className="w-2/3 font-semibold">Tshirt - Wear the code</div>
+              <div className="w-1/3 flex items-center justify-center text-lg">
+                <AiFillMinusCircle className="cursor-pointer text-pink-500" />
+                <span className="mx-2 text-sm">1</span>
+                <AiFillPlusCircle className="cursor-pointer text-pink-500" />
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="item flex my-5">
+              <div className="w-2/3 font-semibold">Tshirt - Wear the code</div>
+              <div className="w-1/3 flex items-center justify-center text-lg">
+                <AiFillMinusCircle className="cursor-pointer text-pink-500" />
+                <span className="mx-2 text-sm">1</span>
+                <AiFillPlusCircle className="cursor-pointer text-pink-500" />
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="item flex my-5">
+              <div className="w-2/3 font-semibold">Tshirt - Wear the code</div>
+              <div className="w-1/3 flex items-center justify-center text-lg">
+                <AiFillMinusCircle className="cursor-pointer text-pink-500" />
+                <span className="mx-2 text-sm">1</span>
+                <AiFillPlusCircle className="cursor-pointer text-pink-500" />
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="item flex my-5">
+              <div className="w-2/3 font-semibold">Tshirt - Wear the code</div>
+              <div className="w-1/3 flex items-center justify-center text-lg">
+                <AiFillMinusCircle className="cursor-pointer text-pink-500" />
+                <span className="mx-2 text-sm">1</span>
+                <AiFillPlusCircle className="cursor-pointer text-pink-500" />
+              </div>
+            </div>
+          </li>
+        </ol>
+        <button className="flex mx-auto mt-16 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded text-sm">
+          <BsFillBagCheckFill className="m-1"/>
+          Checkout
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
