685927bf2094bf1707c40a6fb7b950fc9534dcde
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-09-28 11:25:05 +0530

    creating APIs for getProducts, addProducts, updateproducts

--- models/Product.js
+++ models/Product.js
@@ -13,4 +13,6 @@ const ProductSchema = new mongoose.Schema({
     availableQty: { type: Number, required:true },
 },{ timestamps:true});
 
+
+mongoose.models = {}
 export default mongoose.model("Product", ProductSchema);
