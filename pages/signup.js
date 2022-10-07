6bf1d6c5a96db53660d2ef196ec7f59cbc5a027f
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-10-07 19:11:55 +0530

    creating signup page (functionality)

--- pages/signup.js
+++ pages/signup.js
@@ -1,10 +1,62 @@
-import React from 'react'
-import Link from 'next/link'
+import React, { useState } from "react";
+import Link from "next/link";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Signup = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleChange = (e) => {
+    if (e.target.name == "name") {
+      setName(e.target.value);
+    } else if (e.target.name == "email") {
+      setEmail(e.target.value);
+    } else if (e.target.name == "password") {
+      setPassword(e.target.value);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = { name, email, password };
+    let res = await fetch("http://localhost:3000/api/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    let response = await res.json();
+    console.log(response);
+    setEmail("");
+    setName("");
+    setPassword("");
+
+    toast.success('Your account has been created!', {
+      position: "top-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      });
+  };
   return (
     <div>
-      <div>
+      <ToastContainer
+        position="top-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
@@ -17,15 +69,22 @@ const Signup = () => {
               Sign up to your account
             </h2>
             <p className="mt-2 text-center text-sm text-gray-600">
-              Or{' '}
-              <Link href={'/login'}>
-                <a href="#" className="font-medium text-pink-600 hover:text-pink-500">
+              Or
+              <Link href={"/login"}>
+                <a
+                  href="#"
+                  className="font-medium text-pink-600 hover:text-pink-500"
+                >
                   Login
                 </a>
               </Link>
             </p>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <form
+            onSubmit={handleSubmit}
+            className="mt-8 space-y-6"
+            method="POST"
+          >
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="-space-y-px rounded-md shadow-sm">
               <div>
@@ -33,6 +92,8 @@ const Signup = () => {
                   Name
                 </label>
                 <input
+                  onChange={handleChange}
+                  value={name}
                   id="name"
                   name="name"
                   type="text"
@@ -43,11 +104,13 @@ const Signup = () => {
                 />
               </div>
               <div>
-                <label htmlFor="email-address" className="sr-only">
+                <label htmlFor="email" className="sr-only">
                   Email address
                 </label>
                 <input
-                  id="email-address"
+                  onChange={handleChange}
+                  value={email}
+                  id="email"
                   name="email"
                   type="email"
                   autoComplete="email"
@@ -61,20 +124,8 @@ const Signup = () => {
                   Password
                 </label>
                 <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
-                  placeholder="Password"
-                />
-              </div>
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
+                  onChange={handleChange}
+                  value={password}
                   id="password"
                   name="password"
                   type="password"
@@ -91,18 +142,15 @@ const Signup = () => {
                 type="submit"
                 className="group relative flex w-full justify-center rounded-md border border-transparent bg-pink-600 py-2 px-4 text-sm font-medium text-white hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
               >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-
-                </span>
-                Sign in
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3"></span>
+                Signup
               </button>
             </div>
           </form>
         </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
